f7de54983ce795746a1f4c5ad67b25246d8ed747
hiimvanty <hiimvanty@gmail.com>
2023-09-23 18:41:19 +0700

    helo

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-
-
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')));
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
 app.use(morgan('combined'));
 
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', './views');
-app.set('views', path.join(__dirname,'resource/views'));
+app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`Example app listening on port http://localhost:${port}`);
 });
